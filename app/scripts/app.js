e6692f73d7b165d60bafc33e551ae8b79139d2dd
ALSephirot <asephirotammale@gmail.com>
2016-02-16 23:32:10 -0500

    1st Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hangoutWebAppAngularApp
+ * @description
+ * # hangoutWebAppAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('hangoutWebAppAngularApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
